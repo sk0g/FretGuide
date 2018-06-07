6c898f0217bbe238b277be1289873d0cdb28ae51
Ani Mehta <ani.mehta@griffithuni.edu.au>
2018-06-08 04:20:03 +1000

    Initial commit

---
+++ app/src/main/java/com/example/sk0g/fretguide/TuningSelectorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sk0g.fretguide
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TuningSelectorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tuning_selector)
+    }
+}
