62b469709c0f633cfe276bdaa6a31f62a074ae12
Ani Mehta <tenderanger@gmail.com>
2018-06-13 20:08:20 +1000

    Update variable names, add function to make toasts

--- app/src/main/java/com/example/sk0g/fretguide/WelcomeScreenActivity.kt
+++ app/src/main/java/com/example/sk0g/fretguide/WelcomeScreenActivity.kt
@@ -17,32 +17,38 @@ class WelcomeScreenActivity : AppCompatActivity() {
 
         val context: Context = this
 
-        mRecentTextView = findViewById(R.id.tuning_selector_recent_textview)
+        mRecentTextView = findViewById(R.id.welcome_screen_recent_textview)
         mRecentTextView.setOnClickListener {
             selectRecentTuning(context)
         }
 
-        mPresetTextView = findViewById(R.id.tuning_selector_preset_textview)
+        mPresetTextView = findViewById(R.id.welcome_screen_preset_textview)
         mPresetTextView.setOnClickListener {
             selectPresetTuning(context)
         }
 
-        mCustomTextView = findViewById(R.id.tuning_selector_custom_textview)
+        mCustomTextView = findViewById(R.id.welcome_screen_custom_textview)
         mCustomTextView.setOnClickListener {
             selectCustomTuning(context)
         }
     }
 
     fun selectRecentTuning(context: Context) {
-        Toast.makeText(context,"Recent", Toast.LENGTH_SHORT).show()
+        // TODO: Switch to Recents screen
+        toastText(context,"Recent")
     }
 
     fun selectPresetTuning(context: Context) {
-        Toast.makeText(context,"Present", Toast.LENGTH_SHORT).show()
+        // TODO: Switch to Presets screen
+        toastText(context,"Present")
     }
 
     fun selectCustomTuning(context: Context) {
-        Toast.makeText(context,"Custom", Toast.LENGTH_SHORT).show()
+        // TODO: Switch to Custom Tuning Screen
+        toastText(context,"Custom")
     }
 
+    fun toastText(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
 }
