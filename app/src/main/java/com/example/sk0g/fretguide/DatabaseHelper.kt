e2e84ff2c9368107ae2b1d427023bdc9189814f6
Ani Mehta <tenderanger@gmail.com>
2018-06-27 22:39:49 +1000

    Started work on addTuning(), updated variable and column names

--- app/src/main/java/com/example/sk0g/fretguide/DatabaseHelper.kt
+++ app/src/main/java/com/example/sk0g/fretguide/DatabaseHelper.kt
@@ -1,5 +1,6 @@
 package com.example.sk0g.fretguide
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -8,19 +9,19 @@ import android.database.sqlite.SQLiteQueryBuilder
 class DatabaseHelper : SQLiteOpenHelper {
 
     companion object {
-        val DB_NAME = "RecentTunings.db"
-        val DB_VERSION = 1
+        val DB_NAME     = "RecentTunings.db"
+        val DB_VERSION  = 1
 
-        val tableName   = "tuningsTable"
-        val tuning      = "tuning"
-        val time        = "time"
-        val name        = "name"
-        val type        = "type"
+        val TABLE_NAME      = "tuningsTable"
+        val NOTES_COLUMN    = "NOTES_COLUMN"
+        val TIME_COLUMN     = "TIME_COLUMN"
+        val NAME_COLUMN     = "NAME_COLUMN"
+        val TYPE_COLUMN     = "TYPE_COLUMN"
 
-        private val SQL_CREATE_ENTRIES =
-                "CREATE TABLE IF NOT EXISTS " + tableName + " " + "(" +
-                tuning + " TEXT, " + time + " INTEGER PRIMARY KEY, " +
-                name + " TEXT, " + type + " TEXT );"
+        private val SQL_CREATE_TABLE =
+                "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " " + "(" +
+                NOTES_COLUMN + " TEXT, " + TIME_COLUMN + " INTEGER PRIMARY KEY, " +
+                NAME_COLUMN + " TEXT, " + TYPE_COLUMN + " TEXT );"
     }
 
     var context: Context? = null
@@ -33,7 +34,7 @@ class DatabaseHelper : SQLiteOpenHelper {
 
     override fun onCreate(db: SQLiteDatabase) {
         // TODO: Initialise with values
-        db.execSQL(SQL_CREATE_ENTRIES)
+        db.execSQL(SQL_CREATE_TABLE)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
@@ -44,10 +45,22 @@ class DatabaseHelper : SQLiteOpenHelper {
         var arrayList = ArrayList<TuningModel>()
 
         val builder = SQLiteQueryBuilder()
-        builder.tables = tableName
-        val columns = arrayOf(tuning, time, name, type)
+        builder.tables = TABLE_NAME
+        val columns = arrayOf(NOTES_COLUMN, TIME_COLUMN, NAME_COLUMN, TYPE_COLUMN)
         val rowSelectArgument = arrayOf(keyword)
 
         return arrayList
+    }
+
+    fun addTuning(tuning: TuningModel) {
+        val values = ContentValues()
+
+        values.put(NOTES_COLUMN, tuning.notes)
+        values.put(TIME_COLUMN, tuning.time)
+        values.put(NAME_COLUMN, tuning.name)
+        values.put(TYPE_COLUMN, tuning.type)
+
+        sqlObj.insert(TABLE_NAME, null, values)
+        sqlObj.close()
     }
 }
--- app/src/main/java/com/example/sk0g/fretguide/TuningModel.kt
+++ app/src/main/java/com/example/sk0g/fretguide/TuningModel.kt
@@ -1,13 +1,13 @@
 package com.example.sk0g.fretguide
 
 class TuningModel {
-    var tuning: String?=null
+    var notes: String?=null
     var time: Int?=null
     var type: String?=null
     var name: String?=null
 
-    constructor(tuning: String, time: Int, type: String, name: String) {
-        this.tuning = tuning
+    constructor(notes: String, time: Int, type: String, name: String) {
+        this.notes = notes
         this.time = time
         this.type = type
         this.name = name
