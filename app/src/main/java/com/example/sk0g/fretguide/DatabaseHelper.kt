7fa6bc4ad6e393dc83c22cb9e5d65b518abd47d2
sk0g <ani.mehta@mail.com>
2018-06-27 20:51:38 +1000

    Reworked core SQLite logic, added separate Model file

--- app/src/main/java/com/example/sk0g/fretguide/DatabaseHelper.kt
+++ app/src/main/java/com/example/sk0g/fretguide/DatabaseHelper.kt
@@ -1,20 +1,42 @@
 package com.example.sk0g.fretguide
 
+import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-public class DatabaseHelper : SQLiteOpenHelper {
-    val DATABASE_NAME = "recent_tunings.db"
-    val SCHEMA = 1
-    val TUNING = "tuning"
-    val TIME   = "time"
-    val NAME   = "name"
+class DatabaseHelper : SQLiteOpenHelper {
 
-    @Override fun OnCreate(db: SQLiteDatabase) {
+    companion object {
+        val DB_NAME = "RecentTunings.db"
+        val DB_VERSION = 1
+
+        val tableName   = "tuningsTable"
+        val tuning      = "tuning"
+        val time        = "time"
+        val name        = "name"
+        val type        = "type"
+
+        private val SQL_CREATE_ENTRIES =
+                "CREATE TABLE IF NOT EXISTS " + tableName + " " + "(" +
+                tuning + " TEXT, " + time + " INTEGER PRIMARY KEY, " +
+                name + " TEXT, " + type + " TEXT );"
+    }
+
+    var context: Context? = null
+    var sqlObj: SQLiteDatabase
+
+    constructor(context: Context) : super(context, DB_NAME, null, DB_VERSION) {
+        this.context = context
+        sqlObj = this.writableDatabase
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
         // TODO: Initialise with values
+        db.execSQL(SQL_CREATE_ENTRIES)
     }
 
-    public DatabaseHelper(context: Context) {
-        super(context, DATABASE_NAME, null, SCHEMA)
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        throw RuntimeException("This shouldn't be happening. Implement onUpgrade() pl0x")
     }
+
 }
---
+++ app/src/main/java/com/example/sk0g/fretguide/TuningModel.kt
@@ -0,0 +1,15 @@
+package com.example.sk0g.fretguide
+
+class TuningData {
+    var tuning: String?=null
+    var time: Int?=null
+    var type: String?=null
+    var name: String?=null
+
+    constructor(tuning: String, time: Int, type: String, name: String) {
+        this.tuning = tuning
+        this.time = time
+        this.type = type
+        this.name = name
+    }
+}
