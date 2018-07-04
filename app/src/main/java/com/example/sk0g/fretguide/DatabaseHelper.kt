3d0f5d5e81bff5d43b110ce5de1b2d3bd035e691
Ani Mehta <tenderanger@gmail.com>
2018-07-04 23:44:52 +1000

    Added database schematic, initial values for preset tunings

--- app/src/main/java/com/example/sk0g/fretguide/DatabaseHelper.kt
+++ app/src/main/java/com/example/sk0g/fretguide/DatabaseHelper.kt
@@ -2,12 +2,17 @@ package com.example.sk0g.fretguide
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
-import org.jetbrains.anko.db.ManagedSQLiteOpenHelper
+import org.jetbrains.anko.db.*
 
 class DatabaseHelper(context: Context) : ManagedSQLiteOpenHelper(context, "TuningsDB") {
 
     companion object {
         private var instance: DatabaseHelper? = null
+        private const val tableName  = "tuningsTable"
+        private const val noteColumn = "notes"
+        private const val timeColumn = "time"
+        private const val nameColumn = "name"
+        private const val typeColumn = "type"
 
         @Synchronized
         fun getInstance(context: Context): DatabaseHelper {
@@ -18,8 +23,24 @@ class DatabaseHelper(context: Context) : ManagedSQLiteOpenHelper(context, "Tunin
         }
     }
 
-    override fun onCreate(db: SQLiteDatabase?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreate(db: SQLiteDatabase) {
+        db.createTable(tableName, true,
+                noteColumn to TEXT,
+                timeColumn to INTEGER + PRIMARY_KEY + UNIQUE,
+                nameColumn to TEXT,
+                typeColumn to TEXT)
+
+        db.insert(tableName,
+                noteColumn to "EADGBE",
+                timeColumn to System.currentTimeMillis(),
+                nameColumn to "E Standard",
+                typeColumn to "preset")
+
+        db.insert(tableName,
+                noteColumn to "DADGBE",
+                timeColumn to System.currentTimeMillis(),
+                nameColumn to "Drop D",
+                typeColumn to "preset")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
